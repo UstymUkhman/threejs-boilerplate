258445b25440afa3ba674d6716c6bf3e96f862da
Ustym Ukhman <ustym.ukhman@gmail.com>
2021-05-30 13:13:37 +0300

    Add jest worker mock

--- jest.config.js
+++ jest.config.js
@@ -89,6 +89,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/__mocks__/fileMock.ts',
     '\\.(sass|s?css|less)$': '<rootDir>/tests/__mocks__/styleMock.ts',
+    '\\?worker$': '<rootDir>/tests/__mocks__/workerMock.ts',
 
     '@assets/(.*)': '<rootDir>/src/assets/$1',
     '@scss/(.*)': '<rootDir>/src/scss/$1',
