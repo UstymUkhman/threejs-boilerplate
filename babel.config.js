5d33d723bf5e23978451ba2841e1a1c3c413f90d
Ustym Ukhman <ustym.ukhman@gmail.com>
2021-06-12 11:35:25 +0300

    Add postcss resolution & fix tests

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     test: {
       plugins: [
-        '@babel/plugin-transform-typescript',
         '@babel/plugin-transform-modules-commonjs',
         '@babel/plugin-proposal-export-namespace-from'
       ]
